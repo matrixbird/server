edc8e20446565e2035b19f59876693510b68d30b
hummingbard <mail@hummingbard.com>
2025-03-01 22:57:35 +0600

    x

--- src/api.rs
+++ src/api.rs
@@ -33,28 +33,43 @@ pub async fn transactions(
         //println!("Event: {:#?}", event);
         tracing::info!("Event: {:#?}", event);
 
-        let event_id = event["event_id"].as_str();
-        let room_id = event["room_id"].as_str();
-        let sender = event["sender"].as_str();
-        let event_type = event["type"].as_str();
+        let state_copy = state.clone();
+        let event_copy = event.clone();
 
+        tokio::spawn(async move {
 
-        match (event_id, room_id, sender, event_type) {
-            (Some(event_id), Some(room_id), Some(sender), Some(event_type)) => {
-                if let Err(e) =  state.db.store_event(
-                    event_id,
-                    room_id,
-                    event_type,
-                    sender,
-                    event.clone(),
-                ).await{
-                    tracing::warn!("Failed to store event: {:#?}", e);
+            let event_id = event_copy["event_id"].as_str();
+            let room_id = event_copy["room_id"].as_str();
+            let sender = event_copy["sender"].as_str();
+            let event_type = event_copy["type"].as_str();
+
+            match (event_id, room_id, sender, event_type) {
+                (Some(event_id), Some(room_id), Some(sender), Some(event_type)) => {
+                    if let Err(e) =  state_copy.db.store_event(
+                        event_id,
+                        room_id,
+                        event_type,
+                        sender,
+                        event_copy.clone(),
+                    ).await{
+                        tracing::warn!("Failed to store event: {:#?}", e);
+                    }
+
+                },
+                _ => {
                 }
+            }
+        });
 
-            },
-            _ => {
+        if let Some(event_type) = event["type"].as_str() {
+            if event_type == "matrixbird.email.legacy" {
+                tracing::info!("Outgoing legacy email: {}", event_type);
+            }
+            if event_type == "matrixbird.email.native" {
+                tracing::info!("Outgoing legacy email: {}", event_type);
             }
         }
+
 
 
         let member_event = if let Ok(event) = serde_json::from_value::<RoomMemberEvent>(event.clone()) {
@@ -98,7 +113,6 @@ pub async fn transactions(
             }
             _ => {}
         }
-
 
     }
 
