594a8ec240993d52d3a5b5be820668da94d691bb
hummingbard <mail@hummingbard.com>
2025-03-25 14:31:29 +0600

    cleanup

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -300,31 +300,8 @@ pub async fn transactions(
 
 
         match membership {
-            /*
-            MembershipState::Join => {
-                // Auto-join rooms with user's access token
-                let joined_user = member_event.sender().to_owned();
-                if joined_user != state.appservice.user_id() {
-                    info!("Syncing room: {}", joined_user);
-
-                    let state_clone = state.clone();
-
-                    tokio::spawn(async move {
-                        let _ = tasks::user::join_room(
-                            state_clone, 
-                            joined_user,
-                            room_id,
-                        ).await;
-                    });
-
-
-                    continue;
-                }
-            },
-            */
             MembershipState::Invite => {
 
-                // Auto-join rooms with user's access token
                 let invited_user = member_event.state_key().to_owned();
                 if invited_user != state.appservice.user_id() {
                     continue;
